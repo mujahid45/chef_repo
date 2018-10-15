7d64ba58ae0c8fad68cd01b010c7338ef2cadbf1
mujahid45 <mujahid.dsi@gmail.com>
2018-10-15 13:19:15 +0530

    Update default.rb
    changing the default.rb

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -1,5 +1,17 @@
-#
-# Cookbook:: ntp
+# Cookbook Name:: ntp
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright (c) 2018 The Authors, All Rights Reserved.
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
